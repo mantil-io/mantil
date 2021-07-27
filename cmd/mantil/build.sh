d4032cea34aeedd965ace6ea84495fc5a24fd637
djelusic <djelusic133@gmail.com>
2021-07-27 16:37:39 +0200

    remove unused code

--- cmd/mantil/build.sh
+++ cmd/mantil/build.sh
@@ -3,12 +3,6 @@
 WORK_DIR=~/work
 ASSETS_DIR=$WORK_DIR/mantil-cli/internal/assets
 
-zip -j funcs.zip $WORK_DIR/terraform-aws-modules/funcs/*.tf
-mv funcs.zip $ASSETS_DIR/terraform/modules
-
-zip -j dynamodb.zip $WORK_DIR/terraform-aws-modules/dynamodb/*.tf
-mv dynamodb.zip $ASSETS_DIR/terraform/modules
-
-(cd $ASSETS_DIR && go-bindata -pkg=assets -fs github/ terraform/modules/ terraform/templates/ aws/)
+(cd $ASSETS_DIR && go-bindata -pkg=assets -fs github/ aws/)
 
 (cd $WORK_DIR/mantil-cli/cmd/mantil && go install)
--- internal/aws/aws.go
+++ internal/aws/aws.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"strings"
@@ -14,10 +13,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
 	"github.com/aws/aws-sdk-go-v2/service/lambda"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	s3Types "github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 	stsTypes "github.com/aws/aws-sdk-go-v2/service/sts/types"
-	"github.com/aws/smithy-go"
 )
 
 type AWS struct {
@@ -51,36 +48,6 @@ func (a *AWS) Credentials() (aws.Credentials, error) {
 	return a.config.Credentials.Retrieve(context.TODO())
 }
 
-func (a *AWS) DefaultRegion() string {
-	return a.config.Region
-}
-
-func (a *AWS) CreateS3Bucket(name, region, projectTag string) error {
-	cbi := &s3.CreateBucketInput{
-		Bucket: aws.String(name),
-		CreateBucketConfiguration: &s3Types.CreateBucketConfiguration{
-			LocationConstraint: s3Types.BucketLocationConstraint(region),
-		},
-	}
-	_, err := a.s3Client.CreateBucket(context.TODO(), cbi)
-	if err != nil {
-		return fmt.Errorf("could not create bucket %s in %s - %v", name, region, err)
-	}
-	bti := &s3.PutBucketTaggingInput{
-		Bucket: aws.String(name),
-		Tagging: &s3Types.Tagging{
-			TagSet: []s3Types.Tag{
-				{Key: aws.String("access-project"), Value: aws.String(projectTag)},
-			},
-		},
-	}
-	_, err = a.s3Client.PutBucketTagging(context.TODO(), bti)
-	if err != nil {
-		return fmt.Errorf("could not set bucket tagging - %v", err)
-	}
-	return nil
-}
-
 func (a *AWS) PutObjectToS3Bucket(bucket, key string, object io.Reader) error {
 	poi := &s3.PutObjectInput{
 		Bucket: aws.String(bucket),
@@ -109,115 +76,6 @@ func (a *AWS) GetObjectFromS3Bucket(bucket, key string, o interface{}) error {
 	decoder := json.NewDecoder(rsp.Body)
 	if err := decoder.Decode(&o); err != nil {
 		return err
-	}
-	return nil
-}
-
-func (a *AWS) DeleteObjectFromS3Bucket(bucket, key string) error {
-	doi := &s3.DeleteObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-	}
-
-	_, err := a.s3Client.DeleteObject(context.TODO(), doi)
-	if err != nil {
-		return fmt.Errorf("could not delete key %s in bucket %s - %v", bucket, key, err)
-	}
-	return nil
-}
-
-func (a *AWS) EmptyS3Bucket(name string) error {
-	loi := &s3.ListObjectsV2Input{
-		Bucket: aws.String(name),
-	}
-
-	for {
-		out, err := a.s3Client.ListObjectsV2(context.TODO(), loi)
-		if err != nil {
-			return fmt.Errorf("emptying bucket failed - %v", err)
-		}
-
-		for _, object := range out.Contents {
-			if err := a.DeleteObjectFromS3Bucket(name, *object.Key); err != nil {
-				return fmt.Errorf("emptying bucket failed - %v", err)
-			}
-		}
-
-		if out.IsTruncated {
-			loi.ContinuationToken = out.ContinuationToken
-		} else {
-			break
-		}
-	}
-	return nil
-}
-
-func (a *AWS) DeleteS3Bucket(name string) error {
-	// all objects must be deleted before bucket can be deleted
-	if err := a.EmptyS3Bucket(name); err != nil {
-		return fmt.Errorf("could not delete bucket %s - %v", name, err)
-	}
-
-	dbi := &s3.DeleteBucketInput{
-		Bucket: aws.String(name),
-	}
-
-	_, err := a.s3Client.DeleteBucket(context.TODO(), dbi)
-	if err != nil {
-		return fmt.Errorf("could not delete bucket %s - %v", name, err)
-	}
-	return nil
-}
-
-func (a *AWS) S3BucketExists(name string) (bool, error) {
-	hbi := &s3.HeadBucketInput{
-		Bucket: aws.String(name),
-	}
-
-	_, err := a.s3Client.HeadBucket(context.TODO(), hbi)
-	if err != nil {
-		var oe smithy.APIError
-		if errors.As(err, &oe) {
-			switch oe.ErrorCode() {
-			case "Forbidden":
-				return true, nil
-			case "NotFound":
-				return false, nil
-			case "MovedPermanently":
-				return true, nil
-			default:
-				return false, err
-			}
-		} else {
-			return false, err
-		}
-	}
-	return true, nil
-}
-
-func (a *AWS) UpdateLambdaFunctionCodeFromS3(function, bucket, key string) error {
-	ufci := &lambda.UpdateFunctionCodeInput{
-		FunctionName: aws.String(function),
-		S3Bucket:     aws.String(bucket),
-		S3Key:        aws.String(key),
-	}
-
-	_, err := a.lambdaClient.UpdateFunctionCode(context.TODO(), ufci)
-	if err != nil {
-		return fmt.Errorf("could not update lambda function %s from %s/%s - %v", function, bucket, key, err)
-	}
-	return nil
-}
-
-func (a *AWS) UpdateLambdaFunctionCodeImage(function, image string) error {
-	ufci := &lambda.UpdateFunctionCodeInput{
-		FunctionName: aws.String(function),
-		ImageUri:     aws.String(image),
-	}
-
-	_, err := a.lambdaClient.UpdateFunctionCode(context.TODO(), ufci)
-	if err != nil {
-		return fmt.Errorf("could not update lambda function %s with image %s - %v", function, image, err)
 	}
 	return nil
 }
