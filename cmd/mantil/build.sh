3c719d266d01b8a30a4ba9ed74790c9306169990
Ivan Vlasic <ivan@atoz.technology>
2021-07-28 11:58:03 +0200

    remove awsToken command

--- cmd/mantil/build.sh
+++ cmd/mantil/build.sh
@@ -3,6 +3,6 @@
 WORK_DIR=~/work
 ASSETS_DIR=$WORK_DIR/mantil-cli/internal/assets
 
-(cd $ASSETS_DIR && go-bindata -pkg=assets -fs aws/)
+(cd $ASSETS_DIR && go-bindata -pkg=assets -fs)
 
 (cd $WORK_DIR/mantil-cli/cmd/mantil && go install)
