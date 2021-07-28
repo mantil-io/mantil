a65c7a3f9094d61d236e5a0549f7419a932c98a6
djelusic <djelusic133@gmail.com>
2021-07-28 14:54:59 +0200

    require confirmation before destroying project

--- cmd/mantil/cmd/destroy.go
+++ cmd/mantil/cmd/destroy.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/atoz-technology/mantil-cli/internal/commands/destroy"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -12,9 +13,20 @@ var destroyCmd = &cobra.Command{
 	Use:  "destroy",
 	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		d, err := destroy.New(findProject(args))
+		p, org, path := findProject(args)
+		d, err := destroy.New(p, org, path)
 		if err != nil {
 			log.Fatal(err)
+		}
+		confirmationPrompt := promptui.Prompt{
+			Label: "To confirm deletion, please enter the project name",
+		}
+		projectName, err := confirmationPrompt.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if p.Name != projectName {
+			log.Fatal("Project name doesn't match, exiting...")
 		}
 		if err := d.Destroy(); err != nil {
 			log.Fatal(err)
