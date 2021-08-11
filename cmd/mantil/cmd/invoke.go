70bf041b183c2460f1977abc1fd16dcd7080a4c2
Ivan Vlasic <ivan@atoz.technology>
2021-08-11 12:40:55 +0200

    rename exec command to invoke

--- cmd/mantil/cmd/invoke.go
+++ cmd/mantil/cmd/invoke.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/atoz-technology/mantil-cli/internal/commands/exec"
+	"github.com/atoz-technology/mantil-cli/internal/commands/invoke"
 	"github.com/spf13/cobra"
 )
 
-var execCmd = &cobra.Command{
-	Use:   "exec",
-	Short: "Makes request to functions through project's gateway api.",
+var invokeCmd = &cobra.Command{
+	Use:   "invoke",
+	Short: "Makes requests to functions through project's gateway api.",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		p, _, _, _ := findProject([]string{})
@@ -20,13 +20,13 @@ var execCmd = &cobra.Command{
 		data := cmd.Flag("data").Value.String()
 		endpoint := fmt.Sprintf("%s/%s", p.ApiURL, args[0])
 
-		if err := exec.Endpoint(endpoint, data); err != nil {
+		if err := invoke.Endpoint(endpoint, data); err != nil {
 			log.Fatal(err)
 		}
 	},
 }
 
 func init() {
-	execCmd.Flags().StringP("data", "d", "", "Data for the request")
-	rootCmd.AddCommand(execCmd)
+	invokeCmd.Flags().StringP("data", "d", "", "Data for the request")
+	rootCmd.AddCommand(invokeCmd)
 }
--- internal/commands/invoke/invoke.go
+++ internal/commands/invoke/invoke.go
@@ -1,4 +1,4 @@
-package exec
+package invoke
 
 import (
 	"github.com/atoz-technology/mantil-cli/internal/commands"
