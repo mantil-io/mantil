1c36099de147e636136bdf0c233675e6231203a1
Ivan Vlasic <ivan@atoz.technology>
2021-08-12 16:24:35 +0200

    rename generateApi to generate and add api as child command

---
+++ cmd/mantil/cmd/generate.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/atoz-technology/mantil-cli/internal/commands/generate"
+	"github.com/spf13/cobra"
+)
+
+// generateCmd represents generate command
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "Automatically generate code in the project.",
+}
+
+var generateApiCmd = &cobra.Command{
+	Use:   "api",
+	Short: "Generate new api.",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+		methods, err := cmd.Flags().GetStringSlice("methods")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := generate.Api(name, methods); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("successfuly generated api %s", name)
+	},
+}
+
+func init() {
+	generateApiCmd.Flags().StringSliceP("methods", "m", nil, "Specify additional function methods, if left empty only the root method will be created.")
+	generateCmd.AddCommand(generateApiCmd)
+	rootCmd.AddCommand(generateCmd)
+}
---
+++ internal/commands/generate/generate.go
@@ -0,0 +1,74 @@
+package generate
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/atoz-technology/mantil-cli/internal/generate"
+	"github.com/atoz-technology/mantil-cli/internal/mantil"
+	"golang.org/x/mod/modfile"
+)
+
+func Api(name string, methods []string) error {
+	projectPath, err := mantil.FindProjectRoot(".")
+	if err != nil {
+		return err
+	}
+	importPath, err := findPackageImportPath(projectPath)
+	if err != nil {
+		return err
+	}
+	if err := generateFunctionMain(name, importPath, projectPath); err != nil {
+		return err
+	}
+	return generateApi(name, methods, projectPath)
+}
+
+func findPackageImportPath(projectPath string) (string, error) {
+	modPath := fmt.Sprintf("%s/go.mod", projectPath)
+	buf, err := ioutil.ReadFile(modPath)
+	if err != nil {
+		return "", err
+	}
+	return modfile.ModulePath(buf), nil
+}
+
+func generateFunctionMain(functionName, importPath, projectPath string) error {
+	mainFile := fmt.Sprintf("%s/functions/%s/main.go", projectPath, functionName)
+	if err := generate.GenerateFromTemplate(
+		generate.APIFunctionMainTemplate,
+		&generate.Function{
+			Name:       functionName,
+			ImportPath: importPath,
+		},
+		mainFile,
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+func generateApi(functionName string, methods []string, projectPath string) error {
+	rootFile := fmt.Sprintf("%s/api/%s/root.go", projectPath, functionName)
+	if err := generate.GenerateFromTemplate(
+		generate.APIRootTemplate,
+		&generate.Function{Name: functionName},
+		rootFile,
+	); err != nil {
+		return err
+	}
+	for _, method := range methods {
+		methodFile := fmt.Sprintf("%s/api/%s/%s.go", projectPath, functionName, method)
+		if err := generate.GenerateFromTemplate(
+			generate.APIMethodTemplate,
+			&generate.Method{
+				Name:         method,
+				FunctionName: functionName,
+			},
+			methodFile,
+		); err != nil {
+			return err
+		}
+	}
+	return nil
+}
