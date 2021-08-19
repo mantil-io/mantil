7ded24c6f2005ca154cea1dc9cb7acf3964f21c1
Ivan Vlasic <ivan@atoz.technology>
2021-08-19 13:39:51 +0200

    add parsing of backend logs

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/atoz-technology/mantil-cli
 go 1.16
 
 require (
-	github.com/atoz-technology/mantil.go v0.0.0-20210817122804-7cf25a1a3823
+	github.com/atoz-technology/mantil.go v0.0.0-20210819105739-f2286c34906c
 	github.com/aws/aws-lambda-go v1.26.0 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.8.0
 	github.com/aws/aws-sdk-go-v2/config v1.6.0
@@ -16,14 +16,14 @@ require (
 	github.com/containerd/containerd v1.5.4 // indirect
 	github.com/docker/docker v20.10.7+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/fatih/color v1.12.0 // indirect
+	github.com/fatih/color v1.12.0
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/google/go-github/v37 v37.0.0
 	github.com/klauspost/compress v1.13.4 // indirect
 	github.com/manifoldco/promptui v0.8.0
 	github.com/moby/sys/mount v0.2.0 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
-	github.com/nats-io/jsm.go v0.0.25 // indirect
+	github.com/nats-io/nats-server/v2 v2.3.3-0.20210719165541-e8fea67b1a38 // indirect
 	github.com/nats-io/nats.go v1.11.1-0.20210623165838-4b75fc59ae30
 	github.com/nats-io/nkeys v0.3.0
 	github.com/radovskyb/watcher v1.0.7
@@ -31,7 +31,6 @@ require (
 	github.com/spf13/viper v1.8.1
 	golang.org/x/crypto v0.0.0-20210813211128-0a44fdfbc16e
 	golang.org/x/mod v0.4.2
-	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
--- internal/log/log.go
+++ internal/log/log.go
@@ -1,70 +1,104 @@
 package log
 
 import (
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/fatih/color"
 )
+
+const (
+	levelInfo  = "info"
+	levelDebug = "debug"
+	levelError = "error"
+)
+
+type backendLogLine struct {
+	Level string    `json:"level"`
+	Msg   string    `json:"msg"`
+	Time  time.Time `json:"time"`
+}
 
 var (
 	debugLogLevelEnabled = false
 )
 
 var (
-	infoLog    = color.New()
-	debugLog   = color.New(color.Faint)
-	noticeLog  = color.New(color.FgGreen, color.Bold)
-	backendLog = color.New(color.FgHiBlue)
-	errorLog   = color.New(color.FgRed)
-	fatalLog   = color.New(color.FgRed, color.Bold)
+	infoLog   = color.New()
+	debugLog  = color.New(color.Faint)
+	noticeLog = color.New(color.FgGreen, color.Bold)
+	errorLog  = color.New(color.FgRed)
+	fatalLog  = color.New(color.FgRed, color.Bold)
 )
 
-func Info(msg string, v ...interface{}) {
-	infoLog.PrintlnFunc()(sprintf(msg, v...))
+func Info(format string, v ...interface{}) {
+	infoLog.PrintlnFunc()(sprintf(format, v...))
 }
 
-func Debug(msg string, v ...interface{}) {
+func Debug(format string, v ...interface{}) {
 	if !debugLogLevelEnabled {
 		return
 	}
-	debugLog.PrintlnFunc()(sprintf(msg, v...))
+	debugLog.PrintlnFunc()(sprintf(format, v...))
 }
 
-func Notice(msg string, v ...interface{}) {
-	noticeLog.PrintlnFunc()(sprintf(msg, v...))
+func Notice(format string, v ...interface{}) {
+	noticeLog.PrintlnFunc()(sprintf(format, v...))
 }
 
-func Backend(msg string, v ...interface{}) {
-	backendLog.PrintFunc()(sprintf(msg, v...))
+func Backend(msg string) {
+	l := &backendLogLine{}
+	if err := json.Unmarshal([]byte(msg), &l); err != nil {
+		infoLog.PrintFunc()(sprintf("λ %s", msg))
+		return
+	}
+	if l.Level == levelDebug && !debugLogLevelEnabled {
+		return
+	}
+	c := levelColor(l.Level)
+	c.PrintlnFunc()(sprintf("λ %s", l.Msg))
 }
 
 func Error(err error) {
-	errorLog.PrintlnFunc()(sprintf("%v", err))
+	Errorf(err.Error())
 }
 
-func Errorf(msg string, v ...interface{}) {
-	errorLog.PrintlnFunc()(sprintf(msg, v...))
+func Errorf(format string, v ...interface{}) {
+	errorLog.PrintlnFunc()(sprintf(format, v...))
 }
 
 func Fatal(err error) {
 	fatalLog.PrintlnFunc()(sprintf("%v", err))
 }
 
-func Fatalf(msg string, v ...interface{}) {
-	fatalLog.PrintlnFunc()(sprintf(msg, v...))
+func Fatalf(format string, v ...interface{}) {
+	fatalLog.PrintlnFunc()(sprintf(format, v...))
+}
+
+func sprintf(format string, v ...interface{}) string {
+	return fmt.Sprintf(format, v...)
+}
+
+func levelColor(level string) *color.Color {
+	switch level {
+	case levelInfo:
+		return infoLog
+	case levelDebug:
+		return debugLog
+	case levelError:
+		return errorLog
+	default:
+		return infoLog
+	}
 }
 
 func EnableDebugLogLevel() {
 	debugLogLevelEnabled = true
 }
 
-func sprintf(msg string, v ...interface{}) string {
-	return fmt.Sprintf(msg, v...)
-}
-
 func DisableColor() {
 	noticeLog = color.New(color.Bold)
-	backendLog = color.New(color.Underline)
 	errorLog.DisableColor()
 	fatalLog = color.New(color.Bold)
 }
