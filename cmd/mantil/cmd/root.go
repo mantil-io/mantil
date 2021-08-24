a562d8a295241dcfb6cd33c8a36b940fea431eb8
Ivan Vlasic <ivan@atoz.technology>
2021-08-24 19:42:56 +0200

    remove unused flag

--- cmd/mantil/cmd/root.go
+++ cmd/mantil/cmd/root.go
@@ -21,9 +21,6 @@ var noColor bool
 var rootCmd = &cobra.Command{
 	Use:   "mantil",
 	Short: "Makes serverless development with Go and AWS Lambda joyful",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -35,17 +32,10 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "Config file (default is $HOME/.mantil.yaml)")
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Verbose log output")
 	rootCmd.PersistentFlags().BoolVar(&noColor, "no-color", false, "Don't use color in log output")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
