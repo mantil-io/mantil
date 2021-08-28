865dd6fee96641fc6562c8d6af1a2a75214ba1e1
Ivan Vlasic <ivan@atoz.technology>
2021-08-28 13:33:34 +0200

    fix cli user role name

--- internal/backend/api/security/security.go
+++ internal/backend/api/security/security.go
@@ -23,7 +23,7 @@ func Credentials(project *mantil.Project) (*stsTypes.Credentials, error) {
 	if err != nil {
 		return nil, err
 	}
-	role := fmt.Sprintf("arn:aws:iam::%s:role/mantil-user", accountID)
+	role := fmt.Sprintf("arn:aws:iam::%s:role/mantil-cli-user", accountID)
 	creds, err := aws.RoleCredentials(project.Name, role, policy)
 	if err != nil {
 		return nil, err
