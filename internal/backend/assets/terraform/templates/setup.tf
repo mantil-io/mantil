7ff57bb6d67124bdf31a6a037d35c2ccc22a8b77
Ivan Vlasic <ivan@atoz.technology>
2021-09-10 11:43:47 +0200

    remove ws from setup script

--- internal/backend/assets/terraform/templates/setup.tf
+++ internal/backend/assets/terraform/templates/setup.tf
@@ -31,18 +31,6 @@ locals {
       layers      = ["arn:aws:lambda:eu-central-1:553035198032:layer:git-lambda2:8", "arn:aws:lambda:eu-central-1:477361877445:layer:terraform-lambda:1"]
     }
   }
-  ws_handler = {
-    name        = "ws-handler"
-    s3_key      = "functions/ws-handler.zip"
-    memory_size = 128
-    timeout     = 900
-  }
-  ws_sqs_forwarder = {
-    name        = "ws-sqs-forwarder"
-    s3_key      = "functions/ws-sqs-forwarder.zip"
-    memory_size = 128
-    timeout     = 900
-  }
 }
 
 terraform {
@@ -70,13 +58,6 @@ module "iam" {
   backend_role_arn = module.funcs.role_arn
 }
 
-module "ws" {
-  source        = "http://localhost:8080/terraform/modules/backend-ws.zip"
-  handler       = local.ws_handler
-  sqs_forwarder = local.ws_sqs_forwarder
-  s3_bucket     = local.functions_bucket
-}
-
 # expose aws region and profile for use in shell scripts
 output "aws_region" {
   value = local.aws_region
@@ -98,14 +79,3 @@ output "cli_role" {
   value = module.iam.cli_role
 }
 
-output "ws_url" {
-  value = module.ws.url
-}
-
-output "ws_handler" {
-  value = module.ws.handler
-}
-
-output "ws_sqs_forwarder" {
-  value = module.ws.sqs_forwarder
-}
