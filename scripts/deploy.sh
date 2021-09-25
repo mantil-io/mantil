bd6066892c0e6785251c4fa27116aa8122c5582b
Igor Anic <ianic@inet.hr>
2021-09-25 22:42:31 +0200

    deploy on tag functions to latest bucket key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,8 +22,9 @@ commit=$(git rev-parse --short HEAD)
 dirty=""
 version=$tag
 functions_path="dev/$tag"
+on_tag=0
 # if we are exactly on the tag
-(git describe --exact-match > /dev/null 2>&1 && git diff --quiet) && functions_path="functions/$tag"
+(git describe --exact-match > /dev/null 2>&1 && git diff --quiet) && { functions_path="functions/$tag";on_tag=1; }
 # if local copy is dirty
 (git diff --quiet) || { dirty="$USER";version="$tag-$dirty";functions_path="dev/$version"; }
 if [[ $* == *--use-old-functions-path* ]]; then
@@ -31,7 +32,7 @@ if [[ $* == *--use-old-functions-path* ]]; then
 fi
 echo "> Building cli version: $version"
 go build -o "$GOPATH/bin/mantil" -ldflags "-X main.commit=$commit -X main.tag=$tag -X main.dirty=$dirty -X main.version=$version -X main.functionsPath=$functions_path"
-if [[ "$version" == "$tag" ]]; then
+if [ $on_tag -eq 1 ]; then
    echo "> Releasing new cli version to homebrew"
    cd "$GIT_ROOT"
    (export commit=$commit tag=$tag dirty=$dirty version=$version functionsPath=$functions_path; goreleaser release --rm-dist)
@@ -60,6 +61,9 @@ deploy_function() {
     zip -j -y -q "$1.zip" bootstrap
 
     aws s3 cp "$1.zip" "s3://mantil-downloads/$functions_path/"
+    if [ $on_tag -eq 1 ]; then
+       aws s3 cp "$1.zip" "s3://mantil-downloads/functions/latest/"
+    fi
     rm "$1.zip"
 }
 
