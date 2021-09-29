ff13b11c4d050668f8f1646c7adf80227031e3ba
djelusic <djelusic133@gmail.com>
2021-09-29 13:48:58 +0200

    fix terraform setup test

--- terraform/testdata/setup.tf
+++ terraform/testdata/setup.tf
@@ -54,18 +54,18 @@ provider "aws" {
 }
 
 module "funcs" {
-  source    = "../modules/backend-funcs"
+  source    = "../../modules/backend-funcs"
   functions = local.functions
   s3_bucket = local.functions_bucket
 }
 
 module "iam" {
-  source           = "../modules/backend-iam"
+  source           = "../../modules/backend-iam"
   backend_role_arn = module.funcs.role_arn
 }
 
 module "api" {
-  source           = "../modules/api"
+  source           = "../../modules/api"
   name_prefix      = "mantil"
   functions_bucket = local.functions_bucket
   integrations = [for f in module.funcs.functions :
