2ffcfffe1e23268f7ce7898a8b628896d243606a
Igor Anic <ianic@inet.hr>
2021-10-06 18:45:26 +0200

    simplify cloudformationreason function
    No need for two retrun arguments. Err is never used.
    Add log wraping for errors, to get line numbers in log.

--- aws/cloudformation.go
+++ aws/cloudformation.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+	"github.com/mantil-io/mantil/cli/log"
 )
 
 func (a *AWS) CreateCloudformationStack(name, templateBody string) error {
@@ -20,7 +21,7 @@ func (a *AWS) CreateCloudformationStack(name, templateBody string) error {
 	}
 	cso, err := a.cloudformationClient.CreateStack(context.Background(), csi)
 	if err != nil {
-		return fmt.Errorf("could not create stack %s - %w", name, err)
+		return log.Wrap(err, fmt.Sprintf("could not create stack %s", name))
 	}
 	w := cloudformation.NewStackCreateCompleteWaiter(a.cloudformationClient, func(opts *cloudformation.StackCreateCompleteWaiterOptions) {
 		opts.MinDelay = 10 * time.Second
@@ -30,11 +31,11 @@ func (a *AWS) CreateCloudformationStack(name, templateBody string) error {
 		StackName: aws.String(name),
 	}
 	if err := w.Wait(context.Background(), dsi, 5*time.Minute); err != nil {
-		reason, err := a.cloudformationStackActionFailedReason(aws.ToString(cso.StackId))
-		if err == nil && reason != "" {
-			return fmt.Errorf("could not create stack %s - %s", name, reason)
+		reason := a.cloudformationStackActionFailedReason(aws.ToString(cso.StackId))
+		if reason != "" {
+			return log.Wrap(fmt.Errorf("could not create stack %s - %s", name, reason))
 		}
-		return fmt.Errorf("could not create stack %s", name)
+		return log.Wrap(fmt.Errorf("could not create stack %s", name))
 	}
 	return nil
 }
@@ -45,7 +46,7 @@ func (a *AWS) DeleteCloudformationStack(name string) error {
 	}
 	_, err := a.cloudformationClient.DeleteStack(context.Background(), dsi)
 	if err != nil {
-		return fmt.Errorf("could not delete stack %s - %w", name, err)
+		return log.Wrap(err, fmt.Sprintf("could not delete stack %s", name))
 	}
 	w := cloudformation.NewStackDeleteCompleteWaiter(a.cloudformationClient, func(opts *cloudformation.StackDeleteCompleteWaiterOptions) {
 		opts.MinDelay = 10 * time.Second
@@ -55,11 +56,11 @@ func (a *AWS) DeleteCloudformationStack(name string) error {
 		StackName: aws.String(name),
 	}
 	if err := w.Wait(context.Background(), descsi, 5*time.Minute); err != nil {
-		reason, err := a.cloudformationStackActionFailedReason(name)
-		if err == nil {
-			return fmt.Errorf("could not delete stack %s - %s", name, reason)
+		reason := a.cloudformationStackActionFailedReason(name)
+		if reason != "" {
+			return log.Wrap(fmt.Errorf("could not delete stack %s - %s", name, reason))
 		}
-		return fmt.Errorf("could not delete stack %s", name)
+		return log.Wrap(fmt.Errorf("could not delete stack %s", name))
 	}
 	return nil
 }
@@ -67,22 +68,25 @@ func (a *AWS) DeleteCloudformationStack(name string) error {
 // tries to find reason why stack action failed by going through all the events until first one with status failed is encountered
 // asumption is made that first failed event is also the reason for the failure of the stack action
 // for stacks that do not longer exists (like in the case of create failure) stack id must be provided instead of name
-func (a *AWS) cloudformationStackActionFailedReason(stack string) (string, error) {
+func (a *AWS) cloudformationStackActionFailedReason(stack string) string {
 	dsei := &cloudformation.DescribeStackEventsInput{
 		StackName: aws.String(stack),
 	}
 	dseo, err := a.cloudformationClient.DescribeStackEvents(context.Background(), dsei)
 	if err != nil {
-		return "", err
+		log.Printf("DescribeStackEvents error: %v", err)
+		return ""
 	}
 	if len(dseo.StackEvents) == 0 {
-		return "", fmt.Errorf("could not find reason why stack %s failed", stack)
+		return ""
 	}
 	for i := len(dseo.StackEvents) - 1; i >= 0; i-- {
 		event := dseo.StackEvents[i]
 		if strings.Contains(string(event.ResourceStatus), "FAILED") {
-			return fmt.Sprintf("%s - %s", aws.ToString(event.LogicalResourceId), aws.ToString(event.ResourceStatusReason)), nil
+			return fmt.Sprintf("%s - %s",
+				aws.ToString(event.LogicalResourceId),
+				aws.ToString(event.ResourceStatusReason))
 		}
 	}
-	return "", fmt.Errorf("could not find reason why stack %s failed", stack)
+	return ""
 }
