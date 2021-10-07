d6518c32fa7eead991e5f4decc2d10aa051ba3ba
djelusic <djelusic133@gmail.com>
2021-10-07 17:04:51 +0200

    refactor errors in new command

--- cli/cmd/new.go
+++ cli/cmd/new.go
@@ -5,9 +5,11 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/mantil-io/mantil/cli/log"
 	"github.com/mantil-io/mantil/cli/ui"
 	"github.com/mantil-io/mantil/git"
 	"github.com/mantil-io/mantil/workspace"
+	"github.com/pkg/errors"
 )
 
 var templateRepos = map[string]string{
@@ -47,17 +49,23 @@ func (c *newCmd) run() error {
 	projectPath, _ := filepath.Abs(c.name)
 	repo, err := c.repoURL()
 	if err != nil {
-		return err
+		return log.Wrap(err)
 	}
-	ui.Info("Cloning into %s and replacing import paths with %s", projectPath, c.moduleName)
+	ui.Info("Cloning into %s and replacing import paths with %s...", projectPath, c.moduleName)
 	if err := git.CreateRepo(repo, c.name, c.moduleName); err != nil {
-		return fmt.Errorf("could not clone %s - %v", repo, err)
+		if errors.Is(err, git.ErrRepositoryNotFound) {
+			return log.WithUserMessage(err, c.sourceUserError())
+		}
+		return log.WithUserMessage(
+			err,
+			fmt.Sprintf("Could not initialize repository from source %s: %v", repo, err),
+		)
 	}
 	project := &workspace.Project{
 		Name: c.name,
 	}
 	if err := workspace.SaveProject(project, projectPath); err != nil {
-		return err
+		return log.Wrap(err)
 	}
 	ui.Notice("Done!")
 	ui.Notice("Project initialized at %s", projectPath)
@@ -71,12 +79,25 @@ func (c *newCmd) repoURL() (string, error) {
 	} else {
 		template := c.template()
 		if template == "" {
-			return "", fmt.Errorf("project source recognised as template but it's not one of valid values, can be one of: ping, excuses")
+			return "", log.WithUserMessage(
+				fmt.Errorf("invalid template %s", c.repo),
+				c.sourceUserError(),
+			)
 		}
 		repo = templateRepos[template]
 		ui.Info("Creating project %s from template %s...", c.name, template)
 	}
 	return repo, nil
+}
+
+func (c *newCmd) sourceUserError() string {
+	var validTemplates []string
+	for t := range templateRepos {
+		validTemplates = append(validTemplates, t)
+	}
+	return fmt.Sprintf(`%s is not a valid project source, please provide one of the following:
+- a link to an existing git repository, starting with 'http(s):' or 'git:'
+- the name of a predefined template which can be one of: %s`, c.repo, strings.Join(validTemplates, ", "))
 }
 
 func (c *newCmd) isExternalRepo() bool {
--- git/git.go
+++ git/git.go
@@ -9,7 +9,11 @@ import (
 	"strings"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	"github.com/mantil-io/mantil/cli/log"
 )
+
+var ErrRepositoryNotFound = fmt.Errorf("repository not found")
 
 func CreateRepo(repo, path, moduleName string) error {
 	co := &git.CloneOptions{
@@ -18,14 +22,14 @@ func CreateRepo(repo, path, moduleName string) error {
 	}
 	_, err := git.PlainClone(path, false, co)
 	if err != nil {
-		if err == git.ErrRepositoryAlreadyExists {
-			return fmt.Errorf("local repository already exists")
+		if err == transport.ErrRepositoryNotFound {
+			return log.Wrap(ErrRepositoryNotFound)
 		}
-		return err
+		return log.Wrap(err)
 	}
 	err = os.RemoveAll(fmt.Sprintf("%s/.git", path))
 	if err != nil {
-		return err
+		return log.Wrap(err)
 	}
 	if moduleName == "" {
 		return nil
@@ -42,7 +46,7 @@ func replaceImportPaths(repoDir, old, new string) error {
 	new = strings.ReplaceAll(new, "https://", "")
 	return filepath.Walk(repoDir, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
-			return err
+			return log.Wrap(err)
 		}
 		if info.IsDir() {
 			return nil
@@ -51,12 +55,12 @@ func replaceImportPaths(repoDir, old, new string) error {
 		if strings.HasSuffix(n, ".go") || strings.HasSuffix(n, ".mod") {
 			fbuf, err := ioutil.ReadFile(path)
 			if err != nil {
-				return err
+				return log.Wrap(err)
 			}
 			new := strings.ReplaceAll(string(fbuf), old, new)
 			err = ioutil.WriteFile(path, []byte(new), 0)
 			if err != nil {
-				return err
+				return log.Wrap(err)
 			}
 		}
 		return nil
