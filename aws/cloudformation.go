5b6def4553b7a5debf30bbe55020daa0453ba479
djelusic <djelusic133@gmail.com>
2021-10-26 15:05:08 +0200

    show progress when creating/destroying setup stack

--- aws/cloudformation.go
+++ aws/cloudformation.go
@@ -42,7 +42,8 @@ func (f *CloudFormation) CreateStack(name, templateBody string, resourceTags map
 		}
 		csi.Tags = tags
 	}
-
+	cancel := f.pollStackEvents(name)
+	defer cancel()
 	cso, err := f.cli.CreateStack(context.Background(), csi)
 	if err != nil {
 		return log.Wrap(err, fmt.Sprintf("could not create stack %s", name))
@@ -68,6 +69,8 @@ func (f *CloudFormation) DeleteStack(name string) error {
 	dsi := &cloudformation.DeleteStackInput{
 		StackName: aws.String(name),
 	}
+	cancel := f.pollStackEvents(name)
+	defer cancel()
 	_, err := f.cli.DeleteStack(context.Background(), dsi)
 	if err != nil {
 		return log.Wrap(err, fmt.Sprintf("could not delete stack %s", name))
@@ -93,19 +96,16 @@ func (f *CloudFormation) DeleteStack(name string) error {
 // asumption is made that first failed event is also the reason for the failure of the stack action
 // for stacks that do not longer exists (like in the case of create failure) stack id must be provided instead of name
 func (f *CloudFormation) stackActionFailedReason(stack string) string {
-	dsei := &cloudformation.DescribeStackEventsInput{
-		StackName: aws.String(stack),
-	}
-	dseo, err := f.cli.DescribeStackEvents(context.Background(), dsei)
+	es, err := f.stackEvents(stack, nil)
 	if err != nil {
 		log.Printf("DescribeStackEvents error: %v", err)
 		return ""
 	}
-	if len(dseo.StackEvents) == 0 {
+	if len(es) == 0 {
 		return ""
 	}
-	for i := len(dseo.StackEvents) - 1; i >= 0; i-- {
-		event := dseo.StackEvents[i]
+	for i := len(es) - 1; i >= 0; i-- {
+		event := es[i]
 		if strings.Contains(string(event.ResourceStatus), "FAILED") {
 			return fmt.Sprintf("%s - %s",
 				aws.ToString(event.LogicalResourceId),
@@ -113,4 +113,70 @@ func (f *CloudFormation) stackActionFailedReason(stack string) string {
 		}
 	}
 	return ""
+}
+
+func (f *CloudFormation) pollStackEvents(stack string) func() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func(ctx context.Context) {
+		ts := time.Now()
+		ticker := time.NewTicker(time.Second)
+		for {
+			select {
+			case <-ticker.C:
+				es, _ := f.stackEvents(stack, &ts)
+				for _, e := range es {
+					printStackEvent(e)
+				}
+				if len(es) > 0 {
+					ts = *es[0].Timestamp
+				}
+			case <-ctx.Done():
+				ticker.Stop()
+				return
+			}
+		}
+	}(ctx)
+	return cancel
+}
+
+func (f *CloudFormation) stackEvents(stack string, from *time.Time) ([]types.StackEvent, error) {
+	var events []types.StackEvent
+	var nextToken *string
+	for {
+		dsei := &cloudformation.DescribeStackEventsInput{
+			StackName: aws.String(stack),
+			NextToken: nextToken,
+		}
+		dseo, err := f.cli.DescribeStackEvents(context.Background(), dsei)
+		if err != nil {
+			return nil, log.Wrap(err)
+		}
+		events = append(events, dseo.StackEvents...)
+		if dseo.NextToken == nil {
+			break
+		}
+		nextToken = dseo.NextToken
+	}
+	if from != nil {
+		for idx, e := range events {
+			if e.Timestamp.Before(*from) || e.Timestamp.Equal(*from) {
+				return events[:idx], nil
+			}
+		}
+	}
+	return events, nil
+}
+
+func printStackEvent(e types.StackEvent) {
+	var action string
+	switch e.ResourceStatus {
+	case types.ResourceStatusCreateComplete:
+		action = "Created"
+	case types.ResourceStatusDeleteComplete:
+		action = "Destroyed"
+	}
+	if action == "" {
+		return
+	}
+	fmt.Printf("\t%s %s %s", action, *e.ResourceType, *e.LogicalResourceId)
 }
