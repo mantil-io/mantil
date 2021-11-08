8a3eaea3ad981783f23c26f750aa13fac3f4922e
Igor Anic <ianic@inet.hr>
2021-11-08 15:46:24 +0100

    fix test

--- domain/event_test.go
+++ domain/event_test.go
@@ -24,13 +24,13 @@ func TestEventMarshal(t *testing.T) {
 	buf, err := testCliCommand.Marshal()
 	require.NoError(t, err)
 	require.True(t, gz.IsZiped(buf))
-	require.Len(t, buf, 127)
+	require.Len(t, buf, 130)
 
 	bufUnziped, err := gz.Unzip(buf)
 	require.NoError(t, err)
-	require.Len(t, bufUnziped, 134)
+	require.Len(t, bufUnziped, 141)
 
-	expected := `{"t":1234567890,"v":"v1.2.3","c":"mantil aws install","a":["pero","zdero"],"w":{},"p":{},"s":{},"e":[{"d":{"b":1,"u":2,"m":4,"d":3}}]}`
+	expected := `{"t":1234567890,"v":"v1.2.3","c":"mantil aws install","a":["pero","zdero"],"m":{},"w":{},"p":{},"s":{},"e":[{"d":{"b":1,"u":2,"m":4,"d":3}}]}`
 	require.Equal(t, string(bufUnziped), expected)
 }
 
