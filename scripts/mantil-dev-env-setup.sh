111b5622cfb668a95f202c6d0223fd3101690826
Igor Anic <ianic@inet.hr>
2021-11-09 16:43:59 +0100

    add notes about creating github ssh keys

--- scripts/mantil-dev-env-setup.sh
+++ scripts/mantil-dev-env-setup.sh
@@ -15,3 +15,26 @@ cd ../../team.mantil.com/scripts
 brew bundle
 cd ..
 npm install
+
+
+#
+# /bin/bash -c "$(curl -fsSL https://gist.githubusercontent.com/ianic/26954cbd1f38275f3056db69f6424815/raw/e635a9e55b2d20a987a5bcfbb12556f788cc22fa/dev-env-setup.sh)"
+#
+# ssh-keygen -t ed25519 -C "igor.anic@gmail.com"
+# eval "$(ssh-agent -s)"
+
+# cat > ~/.ssh/config <<EOF
+# Host *
+#   AddKeysToAgent yes
+#   UseKeychain yes
+#   IdentityFile ~/.ssh/id_ed25519
+# EOF
+
+
+# ssh-add -K ~/.ssh/id_ed25519
+
+# pbcopy < ~/.ssh/id_ed25519.pub
+# # dodati na github:
+# # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
+
+# ssh -T git@github.com
