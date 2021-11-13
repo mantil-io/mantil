6e8a8fd314c8bf0e36afca3b4ac43637ac44eadc
Igor Anic <ianic@inet.hr>
2021-11-13 12:42:38 +0100

    add test for kit/token

---
+++ kit/token/token_test.go
@@ -0,0 +1,44 @@
+package token_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/mantil-io/mantil/kit/token"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreate(t *testing.T) {
+	pub, priv, err := KeyPair()
+	require.NoError(t, err)
+	require.Len(t, pub, 43)
+	require.Len(t, priv, 86)
+}
+
+type Claims struct {
+	UID   string `json:"u,omitempty"`
+	Email string `json:"e,omitempty"`
+}
+
+func TestJWT(t *testing.T) {
+	pub, priv, err := KeyPair()
+	require.NoError(t, err)
+
+	c := Claims{
+		UID:   "4ce6e5a0-451b-453a-9b0b-30e037f6029e",
+		Email: "someone@example.com",
+	}
+
+	j, err := JWT(priv, c, time.Hour)
+	require.NoError(t, err)
+
+	fmt.Printf("j: %s\n", j)
+
+	var c2 Claims
+
+	err = Decode(j, pub, &c2)
+	require.NoError(t, err)
+
+	require.Equal(t, c, c2)
+}
