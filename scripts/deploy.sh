9ff8091bf47e545180a84365d9437d7a82e3ce08
Igor Anic <ianic@inet.hr>
2021-11-19 20:19:51 +0100

    try build again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,9 +19,6 @@ tag=$(git describe --always)
 on_tag=0; (git describe --exact-match > /dev/null 2>&1 && git diff --quiet) && { on_tag=1; }
 mantil_bin="$(go env GOPATH)/bin/mantil"
 
-tag=v0.1.24
-on_tag=1
-
 echo "> Building cli with tag=$tag dev=$USER on_tag=$on_tag"
 go build -o "$mantil_bin" -ldflags "-X github.com/mantil-io/mantil/domain.tag=$tag -X github.com/mantil-io/mantil/domain.dev=$USER -X github.com/mantil-io/mantil/domain.ontag=$on_tag" -trimpath
 
