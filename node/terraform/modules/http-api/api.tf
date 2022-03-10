4e95585e3f3c79b570e6e41922e42063b6c9c35d
Ivan Vlasic <ivan@atoz.technology>
2022-03-10 15:14:27 +0100

    disable http api format version 2.0 for HTTP_PROXY

--- node/terraform/modules/http-api/api.tf
+++ node/terraform/modules/http-api/api.tf
@@ -40,7 +40,7 @@ resource "aws_apigatewayv2_route" "http" {
 resource "aws_apigatewayv2_integration" "http" {
   for_each               = local.integrations
   api_id                 = aws_apigatewayv2_api.http.id
-  payload_format_version = "2.0"
+  payload_format_version = each.value.type == "AWS_PROXY" ? "2.0" : "1.0"
   integration_type       = each.value.type
   integration_method     = each.value.integration_method
   integration_uri        = each.value.uri
@@ -58,7 +58,7 @@ resource "aws_apigatewayv2_route" "http_proxy" {
 resource "aws_apigatewayv2_integration" "http_proxy" {
   for_each               = local.integrations
   api_id                 = aws_apigatewayv2_api.http.id
-  payload_format_version = "2.0"
+  payload_format_version = each.value.type == "AWS_PROXY" ? "2.0" : "1.0"
   integration_type       = each.value.type
   integration_method     = each.value.integration_method
   integration_uri        = each.value.type == "AWS_PROXY" ? each.value.uri : "${each.value.uri}/{proxy}"
