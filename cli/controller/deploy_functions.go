8523c05cfa147de8c38317cbf9c865b7f365b596
djelusic <djelusic133@gmail.com>
2022-03-10 12:18:16 +0100

    move env to ExecOptions the env command is not available on windows

--- cli/controller/deploy_functions.go
+++ cli/controller/deploy_functions.go
@@ -91,7 +91,8 @@ func (d *Deploy) localDirs(path string) ([]string, error) {
 func (d *Deploy) buildFunction(name, funcDir string) error {
 	bl := shell.NewBufferedLogger()
 	err := shell.Exec(shell.ExecOptions{
-		Args:         []string{"env", "GOOS=linux", "GOARCH=arm64", "CGO_ENABLED=0", "go", "build", "-o", name, "--tags", "lambda.norpc", "--trimpath"},
+		Args:         []string{"go", "build", "-o", name, "--tags", "lambda.norpc", "--trimpath"},
+		Env:          []string{"GOOS=linux", "GOARCH=arm64", "CGO_ENABLED=0"},
 		WorkDir:      funcDir,
 		Logger:       bl.Logger(),
 		ShowExitCode: false,
